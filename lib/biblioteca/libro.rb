40a45760e173d53c8dff60d4aa5a4231425cfc7b
tania77 <ainatrg@gmail.com>
2015-11-11 19:40:15 +0000

    Mejora de las espectativas

--- lib/biblioteca/libro.rb
+++ lib/biblioteca/libro.rb
@@ -4,7 +4,7 @@ class Libro
 	
 	attr_accessor :titulo, :autor,  :serie, :editorial, :nedicion, :fecha, :isbn10, :isbn13
 	
-	def initialize (titulo, autor, serie, editorial, nedicion, fecha, isbn10, isbn13)
+	def initialize (autor, titulo, serie, editorial, nedicion, fecha, isbn13, isbn10)
 	@titulo = titulo
 	@autor = autor
 	@serie = serie
--- spec/biblioteca_spec.rb
+++ spec/biblioteca_spec.rb
@@ -8,7 +8,11 @@ describe Biblioteca do
     end
     
     before :all do
-        @l1 = Libro.new("titulo", "autor",  "serie", "editorial", "nedicion", "fecha", "isbn10", "isbn13")
+        @l1 = Libro.new("Dave Thomas, Andy Hunt, Chad Fowler.", "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’
+Guide.",nil, "(The Facets of Ruby). Pragmatic Bookshelf;", "4 edition", "(July 7, 2013).", "ISBN-13: 978-1937785499.",
+"ISBN-10: 1937785491.")
+#        @l2 = Libro.new("Scott Chacon.", "Pro Git 2009th Edition. (Pro). Apress; 2009 edition "(August 27, 2009).", "ISBN-13: 978-
+# 1430218333.", "ISBN-10: 1430218339.")
     end
     before :all do 
        @n1 = Nodo.new(@l1, nil) 
@@ -22,25 +26,26 @@ describe Biblioteca do
 #        end
 
         it "debe existir un titulo" do
-            @l1.titulo.should eq("titulo")
+            @l1.titulo.should eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’
+Guide.")
         end
         it "debe existir al menos un autor" do
-            @l1.autor.should eq("autor")
+            @l1.autor.should eq("Dave Thomas, Andy Hunt, Chad Fowler.")
         end
 
         it "debe existir o no una serie" do
-            @l1.serie.should eq("serie") 
+            @l1.serie.should eq nil 
         end
         it "debe existir una editorial" do
-            @l1.editorial.should eq("editorial") 
-            @l1.nedicion.should eq("nedicion") 
+            @l1.editorial.should eq("(The Facets of Ruby). Pragmatic Bookshelf;") 
+            @l1.nedicion.should eq("4 edition") 
         end
         it "debe existir una fecha de publicacion" do
-            @l1.fecha.should eq("fecha") 
+            @l1.fecha.should eq("(July 7, 2013).") 
         end
         it "debe haber almenos un numero isbn" do
-            @l1.isbn10.should eq("isbn10")
-            @l1.isbn13.should eq("isbn13")
+            @l1.isbn10.should eq("ISBN-10: 1937785491.")
+            @l1.isbn13.should eq("ISBN-13: 978-1937785499.")
         end
     end
     
